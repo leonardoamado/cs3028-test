c4d201278964dd42a1ca8a6fb72fae22384e924e
Leonardo Amado <leonardo.amado@acad.pucrs.br>
2024-09-26 13:44:09 +0100

    Now I did not forget to create a branch

--- task3.py
+++ task3.py
@@ -1,1 +1,2 @@
-# Write code that solves task-3
+# Write code solves task-3
+# New code that solves task 4
--- task4.py
+++ task4.py
@@ -1,1 +1,2 @@
 #Here is some code that solves task 4
+#Here is new code that actually solves task 4
