8fa79b8bf90c519f32ba31f071b68d3b3e141c4c
Leonardo Amado <leonardo.amado@acad.pucrs.br>
2024-10-16 08:09:34 +0100

    First version of fibo

---
+++ backend/fibo.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n <= 0:
+        return -1
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(n - 2):
+            a, b = b, a + b
+        return b
